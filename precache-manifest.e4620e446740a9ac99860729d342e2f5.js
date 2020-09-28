46ca95cd3cf3f5ae899f80d34f8de6c5f539add9
Denys Kulibaba <den.kulibab@gmail.com>
2020-09-28 16:57:05 +0300

    Updates

---
+++ precache-manifest.e4620e446740a9ac99860729d342e2f5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "734fcc8831fbe77dc0f26cfccd90c742",
+    "url": "/react-social-network/index.html"
+  },
+  {
+    "revision": "e3f24273c715a80ad3ec",
+    "url": "/react-social-network/static/css/main.be8e054b.chunk.css"
+  },
+  {
+    "revision": "153cb51ff99f823384a4",
+    "url": "/react-social-network/static/js/2.61077f2e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-social-network/static/js/2.61077f2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3f24273c715a80ad3ec",
+    "url": "/react-social-network/static/js/main.06ac823c.chunk.js"
+  },
+  {
+    "revision": "5cc3f8914ecd5df67d39",
+    "url": "/react-social-network/static/js/runtime-main.8da64312.js"
+  },
+  {
+    "revision": "15bd48c389e05451d88ab46ed34cd960",
+    "url": "/react-social-network/static/media/photo.15bd48c3.png"
+  },
+  {
+    "revision": "533626677ece66b384a62a9911a47501",
+    "url": "/react-social-network/static/media/preloader.53362667.svg"
+  }
+]);
